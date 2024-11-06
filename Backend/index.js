1a262fa4ae43d09f4db22b4f8e68b2e5fcaed19a
Aakrisht Srivastava <aakrisri55@gmail.com>
2024-11-06 18:52:30 +0530

    Add Backend

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+const PORT = process.env.PORT | 3000
+
+app.get("/hello", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(PORT , () => {
+    console.log(`Server Running on ${PORT}`)
+})
